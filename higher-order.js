4f2ceb4cc73f35ae394b6f6eb79cc9bbc6853d4d
Francisca <frastengo@gmail.com>
2019-03-08 23:07:13 -0700

    initial commit

--- higher-order.js
+++ higher-order.js
@@ -12,6 +12,8 @@
 const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 // Do not edit the code above.
 
+
+
 /*
   Use the filter method on mixedNumbers to make a new array of just the even numbers.
 
@@ -20,8 +22,13 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
+
+let evenNumbers = mixedNumbers.filter(function(element){
+  return element % 2 === 1
+})
+
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+// let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -30,6 +37,9 @@ let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 // Do not edit the code below.
 const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 // Do not edit the code above.
+
+
+
 
 /*
   Use the map method on the prices array to calculate a new array of post-tax prices.
@@ -44,7 +54,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element){
+  return element * 1.07
+})
+
+// = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -63,7 +77,11 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, cur){
+  return acc + cur;
+})
+
+//  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
